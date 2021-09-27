fdfbb8474db142db025ac3ebdcabbedaa137a656
Niall Maloney <33286803+niall-maloney@users.noreply.github.com>
2021-09-27 21:24:00 +0100

    format

--- scripts/switch-mode.js
+++ scripts/switch-mode.js
@@ -3,20 +3,20 @@ let theme = sessionStorage.getItem('theme');
 
 prefersDark.addEventListener("change", setPreferredMode);
 
-function darkMode(theme){
+function darkMode(theme) {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', theme);
 	document.getElementById("theme-toggle-icon").classList.remove('fa-flip-horizontal');
 }
 
-function lightMode(theme){
+function lightMode(theme) {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', theme);
 	document.getElementById("theme-toggle-icon").classList.add('fa-flip-horizontal');
 }
 
-function setPreferredMode(colorSchemePreference){
-	if(colorSchemePreference.matches) {
+function setPreferredMode(colorSchemePreference) {
+	if (colorSchemePreference.matches) {
 		darkMode('');
 	} else {
 		lightMode('');
@@ -27,9 +27,9 @@ function switchMode() {
 	let theme = sessionStorage.getItem('theme');
 	if (theme === "dark") {
 		lightMode('light');
-	}	else if (theme === "light") {
+	} else if (theme === "light") {
 		darkMode('dark');
-	} else if (prefersDark.matches) {	
+	} else if (prefersDark.matches) {
 		lightMode('light');
 	} else {
 		darkMode('dark');
@@ -41,7 +41,7 @@ if (theme === "dark") {
 	darkMode('dark');
 } else if (theme === "light") {
 	lightMode('light');
-} else if (prefersDark.matches) {	
+} else if (prefersDark.matches) {
 	darkMode('dark');
 } else {
 	lightMode('light');
